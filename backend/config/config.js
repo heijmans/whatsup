2fac87aab62e086e0fd01ab5906313ab48b0f1da
Jan Heijmans <jan@egami.nl>
2018-10-24 12:30:17 +0200

    lint config

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  "development": {
-    "database": "whatsup_dev",
-    "dialect": "postgres",
-    "logging": false,
-    "operatorsAliases": false
+  development: {
+    database: "whatsup_dev",
+    dialect: "postgres",
+    logging: false,
+    operatorsAliases: false,
   },
-  "test": {
-    "database": "whatsup_test",
-    "dialect": "postgres",
-    "logging": false,
-    "operatorsAliases": false
+  test: {
+    database: "whatsup_test",
+    dialect: "postgres",
+    logging: false,
+    operatorsAliases: false,
   },
-  "production": {
-    "database": "whatsup_prod",
-    "dialect": "postgres",
-    "logging": false,
-    "operatorsAliases": false
-  }
+  production: {
+    database: "whatsup_prod",
+    dialect: "postgres",
+    logging: false,
+    operatorsAliases: false,
+  },
 };
