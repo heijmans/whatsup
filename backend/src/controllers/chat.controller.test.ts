45f701d31758dc1ccedecd85475fa7f290b67802
Jan Heijmans <jan@egami.nl>
2018-10-24 12:28:11 +0200

    chat controller test

---
+++ backend/src/controllers/chat.controller.test.ts
@@ -0,0 +1,61 @@
+import { MockModel, MockRouter, toSpy } from "../lib/test-helpers";
+import Chat from "../models/chat.model";
+import chatController from "./chat.controller";
+
+jest.mock("express", () => ({
+  Router: () => new MockRouter(),
+}));
+jest.mock("../models/chat.model", (() => new MockModel()));
+
+const MOCK_CHAT1 = {
+  id: 1,
+  name: "test",
+};
+const MOCK_CHAT2 = {
+  id: 2,
+  name: "other",
+};
+const MOCK_CHATS = [MOCK_CHAT1, MOCK_CHAT2];
+
+describe("chat controller", () => {
+  it("should get all chats", async () => {
+    toSpy(Chat.findAll).mockResolvedValue(MOCK_CHATS);
+    const response = chatController.doGet("/", { });
+    expect(toSpy(Chat.findAll)).toHaveBeenCalledWith();
+
+    await Promise.resolve();
+    expect(response.json).toHaveBeenCalledWith(MOCK_CHATS);
+  });
+
+  it("should create a chat", async () => {
+    toSpy(Chat.create).mockResolvedValue(MOCK_CHAT2);
+    const response = chatController.doPost("/", { body: { name: "ok" } });
+    expect(toSpy(Chat.create)).toHaveBeenCalledWith({ name: "ok" });
+
+    await Promise.resolve();
+    expect(response.json).toHaveBeenCalledWith(MOCK_CHAT2);
+  });
+
+  it("should get a chat", async () => {
+    toSpy(Chat.findById).mockResolvedValue(MOCK_CHAT1);
+    const response = chatController.doGet("/:id", { params: { id: "5" } });
+    expect(toSpy(Chat.findById)).toHaveBeenCalledWith(5);
+
+    await Promise.resolve();
+    expect(response.json).toHaveBeenCalledWith(MOCK_CHAT1);
+  });
+
+  it("should delete a chat", async () => {
+    const mockChat = { destroy: jest.fn().mockResolvedValue(undefined) };
+    
+    toSpy(Chat.findById).mockResolvedValue(mockChat);
+    const response = chatController.doDelete("/:id", { params: { id: "5" } });
+    expect(toSpy(Chat.findById)).toHaveBeenCalledWith(5);
+
+    await Promise.resolve();
+    expect(mockChat.destroy).toHaveBeenCalledWith();
+
+    await Promise.resolve();
+    expect(response.json).toHaveBeenCalledWith({ success: true });
+  });
+});
---
+++ backend/src/lib/test-helpers.ts
@@ -0,0 +1,69 @@
+export function toSpy(x: any): jest.SpyInstance {
+  return x as jest.SpyInstance;
+}
+
+export class MockModel {
+  create = jest.fn();
+  findAll = jest.fn();
+  findById = jest.fn();
+}
+
+export class MockResponse {
+  json = jest.fn();
+  send = jest.fn();
+  status = jest.fn();
+}
+
+export interface IRequest {
+  params?: { [key: string]: string };
+  body?: any;
+}
+
+export type RequestHandler = (req: IRequest, resp: MockResponse) => void;
+
+export class MockRouter {
+  routes: { [key: string]: RequestHandler } = {};
+
+  request(method: string, path: string, fn: RequestHandler) {
+    this.routes[`${method} ${path}`] = fn;
+  }
+
+  get(path: string, fn: RequestHandler) {
+    this.request("GET", path, fn);
+  }
+
+  post(path: string, fn: RequestHandler) {
+    this.request("POST", path, fn);
+  }
+
+  delete(path: string, fn: RequestHandler) {
+    this.request("DELETE", path, fn);
+  }
+
+  doRequest(method: string, path: string, req: IRequest): MockResponse {
+    const response = new MockResponse();
+    const key = `${method} ${path}`;
+    const fn = this.routes[key];
+    if (!fn) {
+      throw new Error(`route not found: ${key}`);
+    }
+    fn(req, response);
+    return response;
+  }
+
+  doGet(path: string, req: IRequest): MockResponse {
+    return this.doRequest("GET", path, req);
+  }
+
+  doPost(path: string, req: IRequest): MockResponse {
+    return this.doRequest("POST", path, req);
+  }
+
+  doDelete(path: string, req: IRequest): MockResponse {
+    return this.doRequest("DELETE", path, req);
+  }
+}
+
+declare module 'express' {
+  function Router(options?: RouterOptions): MockRouter;
+}
