ef12eada75064589c95e1b6bcf0e1b5252b406e1
Jan Heijmans <jan@egami.nl>
2018-10-24 21:46:03 +0200

    working messages

--- frontend/src/components/chat.component.tsx
+++ frontend/src/components/chat.component.tsx
@@ -1,6 +1,8 @@
-import React, { ReactElement } from "react";
+import React, { ChangeEvent, Component, FormEvent, ReactNode } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { AppThunkDispatch } from "../state/actions";
+import { sendMessage } from "../state/chat.actions";
 import { getChat, getMessages } from "../state/selectors";
 import { IChat, IMessage, IState } from "../state/state";
 
@@ -13,26 +15,60 @@ interface IChatConnState {
   messages: IMessage[];
 }
 
-export function Chat({ chat, messages }: IChatProps & IChatConnState): ReactElement<HTMLElement> {
-  if (!chat) {
-    return <h3>Loading...</h3>;
+interface IChatConnActions {
+  sendMessage: (content: string) => void;
+}
+
+interface IChatState {
+  content: string;
+}
+
+export class Chat extends Component<IChatProps & IChatConnState & IChatConnActions, IChatState> {
+  constructor(props: IChatProps & IChatConnState & IChatConnActions) {
+    super(props);
+    this.state = { content: "" };
   }
 
-  return (
-    <div>
-      <Link to="/chats">&lt;&lt; Back</Link>
-      <h1>{chat.name}</h1>
-      <ul>
-        {messages.map((message) => (
-          <li key={message.uuid}>
-            from: {message.from || "you"}
-            <br />
-            {message.content}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  public render(): ReactNode {
+    const { chat, messages } = this.props;
+    if (!chat) {
+      return <h3>Loading...</h3>;
+    }
+
+    const { content } = this.state;
+    return (
+      <div>
+        <Link to="/chats">&lt;&lt; Back</Link>
+        <h1>{chat.name}</h1>
+        <ul>
+          {messages.map((message) => (
+            <li key={message.uuid}>
+              from: {message.from || "you"}
+              <br />
+              {message.content}
+            </li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input onChange={this.handleContentChange} value={content} />
+            <button>Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+
+  private readonly handleContentChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ content: event.currentTarget.value });
+  };
+
+  private readonly handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    const { content } = this.state;
+    this.props.sendMessage(content);
+    this.setState({ content: "" });
+  };
 }
 
 const mapStateToProps = (state: IState, { chatId }: IChatProps): IChatConnState => ({
@@ -40,4 +76,16 @@ const mapStateToProps = (state: IState, { chatId }: IChatProps): IChatConnState 
   messages: getMessages(state, chatId),
 });
 
-export default connect<IChatConnState, {}, IChatProps, IState>(mapStateToProps)(Chat);
+const mapDispatchToProps = (
+  dispatch: AppThunkDispatch,
+  { chatId }: IChatProps,
+): IChatConnActions => ({
+  sendMessage: (content: string) => {
+    dispatch(sendMessage(chatId, content));
+  },
+});
+
+export default connect<IChatConnState, IChatConnActions, IChatProps, IState>(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Chat);
