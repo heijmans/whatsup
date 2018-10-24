040b0677d883ad288496e1b11bf1a7e13a1b0d8c
Jan Heijmans <jan@egami.nl>
2018-10-24 20:20:43 +0200

    app thunk dispatch

--- frontend/src/state/actions.ts
+++ frontend/src/state/actions.ts
@@ -1,4 +1,8 @@
+import { SimpleThunkDispatch } from "../lib/types";
 import { ChatActions } from "./chat.actions";
+import { IState } from "./state";
 import { UserAction } from "./user.actions";
 
 export type AppAction = ChatActions | UserAction;
+
+export type AppThunkDispatch = SimpleThunkDispatch<IState, AppAction>;
